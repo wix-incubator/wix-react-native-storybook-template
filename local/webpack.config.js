70472ddd03dbe072654a138875ed86ac3d1f8aa1
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2017-07-04 16:44:58 +0300

    Server webpack config moved.

--- local/webpack.config.js
+++ local/webpack.config.js
@@ -1,1 +1,4 @@
-module.exports = require('wix-react-native-storybook-server/webpack.config');
+const defaultConfig = require('@storybook/react-native/dist/server/config/webpack.config').default;
+const customConfig = require('wix-react-native-storybook-server/webpack.config');
+
+module.exports = Object.assign({}, defaultConfig, customConfig);
--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,1 +1,4 @@
-module.exports = require('../webpack.config.js');
+const defaultConfig = require('@storybook/react-native/dist/server/config/webpack.config.prod').default;
+const customConfig = require('wix-react-native-storybook-server/webpack.config');
+
+module.exports = Object.assign({}, defaultConfig, customConfig);
