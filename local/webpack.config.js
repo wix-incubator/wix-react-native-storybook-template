b0fffc9d8723cedebf7225de5957eb1befdc82ae
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2017-08-11 09:57:40 +0300

    fixed import again.

--- local/webpack.config.js
+++ local/webpack.config.js
@@ -1,5 +1,5 @@
 const defaultConfig = require('@storybook/react-native/dist/server/config/webpack.config').default;
-const customConfig = require('wix-react-native-storybook-server/local/webpack.config');
+const customConfig = require('wix-react-native-storybook-server/webpack.config');
 
 module.exports = customConfig(defaultConfig);
 
