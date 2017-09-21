62d0e6bd0dd3dbcc3e9f5fd7a29164bc390e950e
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2017-09-21 13:09:38 +0300

    Fixed start issue.

--- addons.js
+++ addons.js
@@ -2,7 +2,7 @@ const React = require('react');
 const addons = require('@storybook/addons').default;
 addons.setPreview(function setPreview() { return React.createElement('div')});
 
-require('react-storybook-addon-docgen/dist/register');
+require('storybook-addon-docgen/register');
 require('./rn-pair/register');
 require('@storybook/addon-knobs/register');
 require('storybook-usage/register');
