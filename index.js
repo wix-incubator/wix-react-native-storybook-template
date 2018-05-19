0689d2d8edc70316ff5935a82dfc4e4a3cbd5df7
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2018-05-19 12:09:51 +0200

    Updated dependency.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 global.STORYBOOK_REACT_CLASSES = {};
 import {configure, getStorybookUI} from '@storybook/react-native';
 
+import storybookTab from './storybook-tab';
+
 import './add-decorators';
 
 export function configureStoriesWithDecorators(resolveFunction, moduleName) {
@@ -10,4 +12,8 @@ export function configureStoriesWithDecorators(resolveFunction, moduleName) {
 export function getStorybook(resolveFunction, moduleName, options = {port: 7007, host: 'localhost'}) {
   configureStoriesWithDecorators(resolveFunction, moduleName);
   return getStorybookUI(options);
+}
+
+export function getStorybookTab(resolveFunction, moduleName, options) {
+  return storybookTab(resolveFunction, moduleName, options);
 }
---
+++ storybook-tab.js
@@ -0,0 +1,74 @@
+import React, {PureComponent} from 'react';
+import {AsyncStorage, View} from 'react-native';
+import {getStorybook} from './index';
+const ASYNC_STORAGE_KEY = 'SELECTED_TAB';
+
+
+export default function getStorybookTab(resolveFunction, module, options = {}) {
+
+  class Storybook extends PureComponent {
+
+    static navigatorStyle = {
+      navBarHidden: true,
+      statusBarHidden: true,
+      statusBarHideWithNavBar: true
+    };
+
+    constructor() {
+      super();
+
+      this.state = {
+        UI: null,
+      };
+
+      fetch('http://localhost:7007')
+        .then(() => {
+          this.setState({
+            UI: getStorybook(
+              storiesGenerator, module, {host: 'localhost', port: '7007', ...options}
+            )()
+          });
+        })
+        .catch(async () => {
+          //eslint-disable-next-line no-console
+          console.log('Storybook server is not running');
+
+          const currentStory = await AsyncStorage.getItem('currentStory');
+
+          const {EventEmitter} = require('events');
+          const channel = new EventEmitter();
+          channel.on('setCurrentStory', (story) => {
+            AsyncStorage.setItem('currentStory', JSON.stringify(story));
+          });
+
+          const addons = require('@storybook/addons').default;
+          addons.setChannel(channel);
+
+          this.setState({
+            UI: getStorybook(() => require('../../storybook/stories/index'), module, {onDeviceUI: true, ...options})(),
+          });
+
+          if (currentStory !== 'null') {
+            channel.emit('setCurrentStory', JSON.parse(currentStory));
+          }
+        });
+    }
+
+    onNavigatorEvent(e) {
+      if (e.id === 'bottomTabSelected') {
+        AsyncStorage.setItem(ASYNC_STORAGE_KEY, e.selectedTabIndex.toString());
+      }
+    }
+
+    render() {
+      return (
+        <View style={{flex: 1}}>
+          <View style={{height: 20, width: '100%', backgroundColor: 'black'}}/>
+          {this.state.UI}
+        </View>
+      );
+    }
+  }
+
+  return Storybook;
+}
