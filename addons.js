194825946c37054f8778a76ed4387b1d33cbd9c0
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2018-05-24 12:11:50 +0300

    removed docgen registration

--- addons.js
+++ addons.js
@@ -2,6 +2,5 @@ const React = require('react');
 const addons = require('@storybook/addons').default;
 addons.setPreview(function setPreview() { return React.createElement('div')});
 
-require('storybook-addon-docgen/register');
 require('@storybook/addon-knobs/register');
 require('storybook-usage/register');
