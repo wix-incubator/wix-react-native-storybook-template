03f87f78cc8a8fb1348a853358364bfdc860336d
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2018-08-06 08:25:29 +0300

    Adding back addons, since ui lib uses them

--- add-decorators.js
+++ add-decorators.js
@@ -1,9 +1,9 @@
-//import {Usage} from 'storybook-usage';
-//import {withKnobsOptions} from '@storybook/addon-knobs';
-//import {addDecorator} from '@storybook/react-native';
+import {Usage} from 'storybook-usage';
+import {withKnobsOptions} from '@storybook/addon-knobs';
+import {addDecorator} from '@storybook/react-native';
 //The order is IMPORTANT, docs must be first!!!
-//addDecorator(Usage);
-//addDecorator(withKnobsOptions({
-//  debounce: {wait: 200, leading: true},
-//  timestamps: true,
-//}));
+addDecorator(Usage);
+addDecorator(withKnobsOptions({
+  debounce: {wait: 200, leading: true},
+  timestamps: true,
+}));
