4adde14d1c52a08203cb03e3c920526fa6673191
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-01-07 13:07:20 +0200

    Removed deprecated call.

--- add-decorators.js
+++ add-decorators.js
@@ -1,10 +1,10 @@
 import {Usage} from 'storybook-usage';
-import {withKnobsOptions} from '@storybook/addon-knobs';
+import {withKnobs} from '@storybook/addon-knobs';
 import {addDecorator} from '@storybook/react-native';
 
 export default () => {
   addDecorator(Usage);
-  addDecorator(withKnobsOptions({
+  addDecorator(withKnobs({
     debounce: {wait: 200, leading: true},
     timestamps: true,
   }));
