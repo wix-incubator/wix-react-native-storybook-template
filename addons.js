c45591fec096966e098dc8f6a2fd94eca1f7e7d4
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-01-21 14:41:17 +0200

    Fixed preview in addons.

--- addons.js
+++ addons.js
@@ -1,6 +1,4 @@
 const React = require('react');
-const addons = require('@storybook/addons').default;
-addons.setPreview(function setPreview() { return React.createElement('div')});
 
 require('@storybook/addon-knobs/register');
 require('storybook-usage/register');
