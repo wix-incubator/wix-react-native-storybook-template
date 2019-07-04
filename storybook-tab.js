7effb4de89a1af6ea5cf2edd2944991bfed621e8
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-07-04 11:19:16 +0300

    removed async storage.

--- storybook-tab.js
+++ storybook-tab.js
@@ -1,10 +1,7 @@
 import React, {PureComponent} from 'react';
-import {AsyncStorage, View} from 'react-native';
 import {getStorybook} from './get-storybook';
 
 import './rn-addons';
-
-const ASYNC_STORAGE_KEY = 'SELECTED_TAB';
 
 export default function getStorybookTab(resolveFunction, module, options) {
   const StorybookUI = getStorybook(resolveFunction, module, options);
@@ -21,12 +18,6 @@ export default function getStorybookTab(resolveFunction, module, options) {
           drawBehind: true,
         }
       };
-    }
-
-    onNavigatorEvent(e) {
-      if (e.id === 'bottomTabSelected' && e.selectedTabIndex !== undefined && e.selectedTabIndex !== null) {
-        AsyncStorage.setItem(ASYNC_STORAGE_KEY, e.selectedTabIndex.toString());
-      }
     }
 
     render() {
