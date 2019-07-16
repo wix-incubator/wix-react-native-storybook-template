ee16ae1bcfbd0b6ae6f5e68495a9c6a85eafc3f6
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-07-16 10:53:46 +0300

    Added silence for async storage, usage addon in react-native side.

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ import storybookTab from './storybook-tab';
 export {configureStoriesWithDecorators, getStorybook} from './get-storybook';
 
 export function getStorybookTab(resolveFunction, moduleName, options) {
+  const ReactNative = require('react-native');
+  Object.defineProperty(ReactNative, 'AsyncStorage', {
+    get() {
+      return require('@react-native-community/async-storage').default;
+    },
+  });
+
   return storybookTab(resolveFunction, moduleName, options);
 }
