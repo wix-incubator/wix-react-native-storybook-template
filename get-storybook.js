e6ed3ac4977503921106e3b54f73b4ed561823e7
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-07-18 15:07:15 +0300

    removed accidental call.

--- get-storybook.js
+++ get-storybook.js
@@ -1,8 +1,6 @@
 import {configure, getStorybookUI} from '@storybook/react-native';
-import addDecorators from './add-decorators';
 
 export function configureStoriesWithDecorators(resolveFunction, moduleName) {
-  addDecorators();
   configure(resolveFunction, moduleName);
 }
 
