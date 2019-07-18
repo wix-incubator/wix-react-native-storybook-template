fc5d250f01dac0778de8b7b374caed3a0cd949de
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-07-18 20:38:46 +0300

    Temporary testId wrapper.

--- add-decorators.js
+++ add-decorators.js
@@ -1,8 +1,13 @@
+import * as React from 'react';
+import {View} from 'react-native';
 import {Usage} from 'storybook-usage';
 import {withKnobs} from '@storybook/addon-knobs';
 
 export default (storiesOf) => {
   return storiesOf
+    .addDecorator((storyFn, context) => (
+      <View testID={context.id}>{storyFn()}</View>
+    ))
     .addDecorator(Usage)
     .addDecorator(withKnobs({
       debounce: {wait: 200, leading: true},
