76b50c4b0d080e6ca8df072b47dd9d418ac416a5
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-07-18 14:08:59 +0300

    Configure decorators as local.

--- add-decorators.js
+++ add-decorators.js
@@ -1,11 +1,11 @@
 import {Usage} from 'storybook-usage';
 import {withKnobs} from '@storybook/addon-knobs';
-import {addDecorator} from '@storybook/react-native';
 
-export default () => {
-  addDecorator(Usage);
-  addDecorator(withKnobs({
-    debounce: {wait: 200, leading: true},
-    timestamps: true,
-  }));
+export default (storiesOf) => {
+  return storiesOf
+    .addDecorator(Usage)
+    .addDecorator(withKnobs({
+      debounce: {wait: 200, leading: true},
+      timestamps: true,
+    }));
 }
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import storybookTab from './storybook-tab';
 
+import {storiesOf as storybookStoriesOf} from '@storybook/react-native';
 export {configureStoriesWithDecorators, getStorybook} from './get-storybook';
+
+export function storiesOf(...args) {
+  return addDecorators(
+    storybookStoriesOf(...args)
+  );
+}
 
 export function getStorybookTab(resolveFunction, moduleName, options) {
   const ReactNative = require('react-native');
