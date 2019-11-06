8dcfde0519bf67d1abb0facc123b4fec4210fe0f
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-11-06 18:09:48 +0200

    updating dependencies.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {storiesOf as storybookStoriesOf} from '@storybook/react-native';
 
 import storybookTab from './storybook-tab';
+import storybookTabNoStyle from './storybook-tab-no-style';
 import addDecorators from './add-decorators';
 
 export {configureStoriesWithDecorators, getStorybook} from './get-storybook';
@@ -12,6 +13,18 @@ export function storiesOf(...args) {
     storybookStoriesOf(...args)
   );
 }
+
+export function getStorybookTabNoStyle(resolveFunction, moduleName, options) {
+  const ReactNative = require('react-native');
+  Object.defineProperty(ReactNative, 'AsyncStorage', {
+    get() {
+      return require('@react-native-community/async-storage').default;
+    },
+  });
+
+  return storybookTabNoStyle(resolveFunction, moduleName, options);
+}
+
 
 export function getStorybookTab(resolveFunction, moduleName, options) {
   const ReactNative = require('react-native');
--- storybook-tab-no-style.js
+++ storybook-tab-no-style.js
@@ -7,19 +7,6 @@ export default function getStorybookTab(resolveFunction, module, options) {
   const StorybookUI = getStorybook(resolveFunction, module, options);
 
   class Storybook extends PureComponent {
-    static get options() {
-      return {
-        statusBar: {
-          visible: false,
-          drawBehind: true,
-        },
-        topBar: {
-          visible: false,
-          drawBehind: true,
-        }
-      };
-    }
-
     render() {
       return (<StorybookUI/>);
     }
